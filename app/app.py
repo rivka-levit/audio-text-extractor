981c33eca02789a7d8ad386f8fd9d497aa3d64aa
rivka-yuliia <juliacreadora@gmail.com>
2024-01-10 17:25:59 +0200

    test redirect to error page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 import os
+import json
 
 import psycopg2
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 from datetime import datetime, timezone
 from pathlib import Path
@@ -55,14 +56,14 @@ def index():
 
     if request.method == 'POST':
         if 'audio' not in request.files:
-            context = {'message': 'No file part'}
-            return render_template('error.html', **context)
+            message = json.dumps({'message': 'No file part'})
+            return redirect(url_for('error_page', message=message))
 
         audio = request.files['audio']
 
         if audio.filename == '':
-            context = {'message': 'No selected file'}
-            return render_template('error.html', **context)
+            message = json.dumps({'message': 'No selected file'})
+            return redirect(url_for('error_page', message=message))
 
         if audio and allowed_file(audio.filename):
             ext = audio.filename.rsplit('.', 1)[1]
@@ -91,7 +92,8 @@ def index():
 
 @app.route('/error', methods=['GET'])
 def error_page():
-    return render_template('error.html')
+    message = request.args.get('message', None)
+    return render_template('error.html', message=json.loads(message))
 
 
 @app.route('/extract', methods=['GET'])
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from app import app
 
+import io
 
 class TestApp(TestCase):
     """Tests for the Flask app."""
@@ -23,3 +24,31 @@ class TestApp(TestCase):
 
         self.assertEqual(r.status_code, 200)
         self.assertIn(header, r.get_data(as_text=True))
+
+    def test_index_post_without_audio_return_error_page(self):
+        """Test post to index page without audio redirects to error page."""
+
+        r = self.client.post('/', follow_redirects=True,
+                             content_type='multipart/form-data')
+
+        expected_msg = 'No file part'
+        expected_path = '/error'
+
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.request.path, expected_path)
+        self.assertIn(expected_msg, r.get_data(as_text=True))
+
+    def test_index_post_with_empty_audio_name_return_error_page(self):
+        """Test post to index page with empty audio name return error page."""
+
+        data = {'audio': (io.BytesIO(b"abcdef"), '')}
+
+        r = self.client.post('/', data=data, follow_redirects=True,
+                             content_type='multipart/form-data')
+
+        expected_msg = 'No selected file'
+        expected_path = '/error'
+
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.request.path, expected_path)
+        self.assertIn(expected_msg, r.get_data(as_text=True))
